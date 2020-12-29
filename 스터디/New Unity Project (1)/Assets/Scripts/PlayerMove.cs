80afd8d58813cdfeba982b4a458d16df26bdc064
Chaewony <tcd1205@naver.com>
2020-12-29 13:35:15 +0900

    12/29

---
+++ 스터디/New Unity Project (1)/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float maxSpeed;
+    public float jumpPower;
+    Rigidbody2D rigid;
+    SpriteRenderer SpriteRenderer;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Attack
+        if (Input.GetButtonDown("Attack"))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        if (Input.GetButtonUp("Attack"))
+        {
+            anim.SetBool("isAttacking", false);
+        }
+
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            anim.SetBool("isJumping", true);
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+        //Direction Sprite
+        if (Input.GetButtonDown("Horizontal"))
+            SpriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        //Animation
+        if (rigid.velocity.normalized.x == 0)
+            anim.SetBool("isWalking", false);
+        else
+            anim.SetBool("isWalking", true);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Move Speed
+        float h = Input.GetAxisRaw("Horizontal");
+        rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        //Max Speed
+        if (rigid.velocity.x > maxSpeed) //Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed * (-1)) //Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+
+        //Landing Platform
+        //Debug.DrawRay(rigid.position, Vector2.down, new Color(0, 1, 0));
+
+        //RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1);
+
+        //if(rayHit.collider != null)
+        {
+            //Debug.Log(rayHit.collider.name);
+        }
+    }
+}
